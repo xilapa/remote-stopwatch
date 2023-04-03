4a2fbf95fd86453b17fd41c2b46b42cd1ebabaf0
xilapa <junior.dirceu@pm.me>
2023-04-03 20:47:31 -0300

    stopwatch page and server

--- go.mod
+++ go.mod
@@ -8,4 +8,8 @@ require (
 	github.com/xilapa/go-tiny-projects/test-assertions v0.0.0-20230131013927-46df9924699c
 )
 
-require github.com/google/go-cmp v0.5.9 // indirect
+require (
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/klauspost/compress v1.10.3 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+)
--- main.go
+++ main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"net/http"
+	"regexp"
 	"text/template"
+	"time"
 
 	cmap "github.com/orcaman/concurrent-map/v2"
 	sw "github.com/xilapa/remote-stopwatch/stopwatch"
+	"github.com/xilapa/remote-stopwatch/wsobserver"
+	"nhooyr.io/websocket"
 )
 
 var (
-	templates  = template.Must(template.ParseFiles("pages/home.html"))
-	stopwatchs = cmap.New[*sw.StopWatch]()
+	templates   = template.Must(template.ParseFiles("pages/home.html", "pages/stopwatch.html"))
+	stopwatchs  = cmap.New[*sw.StopWatch]()
+	idValidator = regexp.MustCompile("^/(join|syncwatch)/([A-Za-z0-9_-]+)$")
 )
 
 func main() {
 	http.HandleFunc("/", home)
 	http.HandleFunc("/create", create)
+	http.HandleFunc("/join/", join)
+	http.HandleFunc("/syncwatch/", syncwatch)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -27,4 +34,56 @@ func create(w http.ResponseWriter, r *http.Request) {
 	stopwatch := sw.NewStopWatch()
 	stopwatchs.Set(stopwatch.Id, stopwatch)
 	http.Redirect(w, r, "/join/"+stopwatch.Id, http.StatusFound)
+}
+
+type observerView struct {
+	ObserversCount int
+	Id             string
+	CurrentTime    time.Duration
+}
+
+func join(w http.ResponseWriter, r *http.Request) {
+	stopwatch := getStopwatchFromPath(r.URL.Path)
+	if stopwatch == nil {
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+
+	view := observerView{
+		ObserversCount: stopwatch.ObserversCount(),
+		Id:             stopwatch.Id,
+		CurrentTime:    stopwatch.CurrentTime,
+	}
+
+	templates.ExecuteTemplate(w, "stopwatch.html", view)
+}
+
+func syncwatch(w http.ResponseWriter, r *http.Request) {
+	c, err := websocket.Accept(w, r, nil)
+	if err != nil {
+		return
+	}
+
+	stopwatch := getStopwatchFromPath(r.URL.Path)
+
+	// just for testing
+	stopwatch.Start()
+	if stopwatch == nil {
+		c.Close(websocket.StatusNormalClosure, "stopwatch not found")
+		return
+	}
+
+	obs := wsobserver.NewWebSocketObserver(r.Context(), c)
+	stopwatch.Add(obs)
+}
+
+func getStopwatchFromPath(path string) *sw.StopWatch {
+	idMatch := idValidator.FindStringSubmatch(path)
+	if idMatch == nil || len(idMatch) != 3 {
+		return nil
+	}
+	stopWatch, ok := stopwatchs.Get(idMatch[2])
+	if !ok {
+		return nil
+	}
+	return stopWatch
 }
