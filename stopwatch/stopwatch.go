f06472f0486203ec391ca6584b6f9335a44ced9c
xilapa <junior.dirceu@pm.me>
2023-04-07 10:59:53 -0300

    stopwatch observer interface moved to another file

--- stopwatch/stopwatch.go
+++ stopwatch/stopwatch.go
@@ -17,21 +17,6 @@ func mustCreateNanoIdGen() func() string {
 	return nanoIdGen
 }
 
-// Observer is something that wants to
-// listens to the stopwatch.
-type Observer interface {
-	// HandleNewTime is called when the stopwatch report a new time frame.
-	// This method should not block due to the fact that the stopwatch
-	// is blocked until all observers are done. Also, this method is called
-	// with high frequency, so it should be as fast as possible.
-	HandleNewTime(t time.Duration)
-
-	// HandleReset is called when the stopwatch is reset.
-	// This method should block until the observer is done. Because the
-	// observers must know that the stopwatch is reset.
-	HandleReset()
-}
-
 type StopWatch struct {
 	Id               string             // id of the stopwatch
 	CurrentTime      time.Duration      // current time of the stopwatch
---
+++ stopwatch/stopwatch_observer.go
@@ -0,0 +1,18 @@
+package stopwatch
+
+import "time"
+
+// Observer is something that wants to
+// listens to the stopwatch.
+type Observer interface {
+	// HandleNewTime is called when the stopwatch report a new time frame.
+	// This method should not block due to the fact that the stopwatch
+	// is blocked until all observers are done. Also, this method is called
+	// with high frequency, so it should be as fast as possible.
+	HandleNewTime(t time.Duration)
+
+	// HandleReset is called when the stopwatch is reset.
+	// This method should block until the observer is done. Because the
+	// observers must know that the stopwatch is reset.
+	HandleReset()
+}
